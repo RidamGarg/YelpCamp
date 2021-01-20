f5cafe57cea2dd44f81ffab2c52bd18d573715d7
Ridam Garg <ridamgarg8979@gmail.com>
2021-01-21 01:40:31 +0530

    Deploying the App

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 const port = process.env.PORT||3000 ;
-app.listen('3000',()=>{
+app.listen(port,()=>{
     console.log('Listening');
 })
 
